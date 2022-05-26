f1f10d95baef9abc87b197f7c23445c9129d3172
Tiyarong Gurung <71687703+tiyaronggurung@users.noreply.github.com>
2022-05-26 13:36:20 -0400

    Add files via upload

---
+++ display.js
@@ -0,0 +1,53 @@
+
+class SLLNode {
+    constructor(data) {
+        this.value = data; 
+        this.next = null;
+    }
+}
+
+class SLL { 
+    constructor() { 
+        this.head = null; 
+    }
+
+    addFront(val) {
+        var createNode = new SLLNode(val); 
+        createNode.next = this.head; 
+        this.head = createNode; 
+        return this.head; 
+    }
+
+    removeFront() {
+        if (!this.head) {
+            return this.head;
+        }
+        var deleteNode = this.head; 
+        this.head = deleteNode.next;
+        deleteNode.next = null;
+        return this.head;
+    }
+
+    front() {  
+        if (!this.head) {
+            return null;
+        } else {
+            return this.head.value;
+        }
+    }
+
+
+    display() {
+        var string = ""; 
+        if (!this.head) {
+            return ""; 
+        }
+        string += this.head.value; 
+        var runner = this.head.next;
+        while (runner != null) {
+            str += ", " + runner.value; 
+            runner = runner.next; 
+        return str;
+        }
+    }
+}
